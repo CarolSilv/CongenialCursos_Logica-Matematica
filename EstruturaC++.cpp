d91fb53009ffb0832777d8b97e901cec521ea40b
Carolayne <91797824+CarolSilv@users.noreply.github.com>
2022-07-27 19:37:23 -0300

    First commit in C++

---
+++ EstruturaC++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    cout << "Welcome ";
+    cout << 5 + 5 << " ";
+    int x = 3*3;
+    cout << x << endl;
+
+    return 0;
+}
---
+++ HelloC++.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *_argv[])
+{
+    cout << "Hello, world!";
+    return 0;
+}
